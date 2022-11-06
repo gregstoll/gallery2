ee33a21fb7c5001477a3829d17cdc0fa953367da
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2022-11-06 17:45:13 +0100

    Fix UtilitiesTest.testHtmlSafe test.

--- lib/pear/Safe.php
+++ lib/pear/Safe.php
@@ -366,8 +366,9 @@ class HTML_Safe
                    }
                 }
 
-                $tempval = preg_replace('/&#(\d+);?/me', "chr('\\1')", $value); //"'
-                $tempval = preg_replace('/&#x([0-9a-f]+);?/mei', "chr(hexdec('\\1'))", $tempval);
+                $tempval = preg_replace_callback('/&#(\d+);?/m',
+		    function ($matches) {return chr(hexdec($matches[0]));},
+		    $value);
 
                 if ((in_array($name, $this->protocolAttributes)) && 
                     (strpos($tempval, ':') !== false)) 
